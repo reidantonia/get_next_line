306217855dd6796683ab703a9472c1d589587531
Antonia Reid <antoniareid@gmail.com>
2017-12-21 20:44:12 +0000

    adding

--- get_next_line.c
+++ get_next_line.c
@@ -13,35 +13,73 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-int get_next_line(const int fd, char **line)
+static int		check_line(char *str)
 {
-	char buf[BUFF_SIZE + 1];
-	int count;
-	char c;
-	int i;
-	ssize_t ret;
-	count = 0;
-	i = 0;
-	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
-	{
-		if (ret == -1)
-			return (-1);
-		buf[BUFF_SIZE] = '\0';
-		printf("Ret is '%lu'\nfd is '%d'\n",ret,fd);
-		printf("%d is <<%s>>\n\n", i, buf);
-		i++;
-	}
+		int i;
 
-	printf("\n\nTHe result is:\n<<%s>>", buf);
-	/* {
-	   printf("%c", c);
-	   count++;
-	   }*/
-	*line = ft_strdup(buf);
-	if (ret == 1)
-		return (1);
-	//	if (ret == EOF)
-	//	return (0);
-	else
-		return (-1);
+		i = 0;
+		while (str)
+		{
+				if (str[i] == '\n')
+						return (1);
+				i++;
+		}
+		return (0);
+}
+
+static char		*trim_line(char *str)
+{
+		char	*ret;
+		int		i;
+
+		i = 0;
+		while (str[i] != '\n')
+				i++;
+		ret = ft_strnew(i);
+		i = 0;
+		while (str[i] != '\n')
+		{
+				ret[i] = str[i];
+				i++;
+		}
+		ret[i] = '\0';
+		return (ret);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+		char buf[BUFF_SIZE + 1];
+		char *str;
+		char c;
+		ssize_t ret;
+		
+		while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+		{
+				if (ret == -1)
+						return (-1);
+				buf[BUFF_SIZE] = '\0';
+				if (check_line(buf))
+				{
+						*line = trim_line(buf);
+						return (0);
+				}
+				else
+				{
+						str = ft_strjoin(str, buf);
+				}
+				if (check_line(buf))
+				{
+						*line = trim_line(buf);
+						return (0);
+				}
+				while (buf)
+						buf[i++] = '\0';
+				i++;
+		}
+		if (ret == 1)
+				return (1);
+		//	if (ret == EOF)
+		//	return (0);
+		else
+				return (-1);
 }
