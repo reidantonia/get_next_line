fa0083b325a44b3d5c95db24f6bb2df48ce48dce
reidantonia <antoniareid@gmail.com>
2018-02-03 12:30:54 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: areid <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/28 09:36:52 by areid             #+#    #+#             */
-/*   Updated: 2017/11/28 09:37:07 by areid            ###   ########.fr       */
+/*   Created: 2018/02/03 10:20:57 by areid             #+#    #+#             */
+/*   Updated: 2018/02/03 10:21:34 by areid            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,88 +14,89 @@
 
 static int		ft_chkl(const char *str)
 {
-		int i;
+	int i;
 
-		i = 0;
-		while (str && str[i] != '\0')
-		{
-				if (str[i] == '\n')
-						return (1);
-				i++;
-		}
-		return (0);
+	i = 0;
+	while (str && str[i] != '\0')
+	{
+		if (str[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
 static char		*ft_trim_line(char *str)
 {
-		char	*ret;
-		int		i;
+	char	*ret;
+	int		i;
 
-		i = 0;
-		while (str[i] != '\n' && str[i] != '\0')
-				i++;
-		ret = ft_strnew(i);
-		i = 0;
-		while (str[i] != '\n' && str[i] != '\0')
-		{
-				ret[i] = str[i];
-				i++;
-		}
-		return (ret);
+	i = 0;
+	while (str[i] != '\n' && str[i] != '\0')
+		i++;
+	ret = ft_strnew(i);
+	i = 0;
+	while (str[i] != '\n' && str[i] != '\0')
+	{
+		ret[i] = str[i];
+		i++;
+	}
+	return (ret);
 }
 
 static char		*ft_get_remainder(char *str)
 {
-		int		i;
-		char	*ret;
+	int		i;
+	char	*ret;
 
-		i = 0;
-		if (str[i] == '\0')
-				return (NULL);
-		while (str[i] != '\n' && str[i] != '\0')
-				i++;
-		ret = ft_strsub(str, i + 1, ft_strlen(str) - i);
-		free(str);
-		return (ret);
+	i = 0;
+	if (str[i] == '\0')
+		return (NULL);
+	while (str[i] != '\n' && str[i] != '\0')
+		i++;
+	ret = ft_strsub(str, i + 1, ft_strlen(str) - i);
+	free(str);
+	return (ret);
 }
 
 char			*ft_really_get(int const fd, char **line, char **str)
 {
-		char		buf[BUFF_SIZE + 1];
-		int			ret;
-		char		*ptr;
-		while (!(ft_chkl((const char*)str[fd])) && (ret = read(fd, buf, BUFF_SIZE)) > 0)
-		{
-				buf[ret] = '\0';
-				ptr = str[fd];
-				str[fd] = ft_strjoin(ptr, buf);
-				free(ptr);
-		}
-		*line = ft_trim_line(str[fd]);
-		return (str[fd]);
+	char		buf[BUFF_SIZE + 1];
+	int			ret;
+	char		*ptr;
+
+	while (!(ft_chkl((const char*)str[fd]))
+			&& (ret = read(fd, buf, BUFF_SIZE)) > 0)
+	{
+		buf[ret] = '\0';
+		ptr = str[fd];
+		str[fd] = ft_strjoin(ptr, buf);
+		free(ptr);
+	}
+	*line = ft_trim_line(str[fd]);
+	return (str[fd]);
 }
 
-
-int					get_next_line(int const fd, char **line)
+int				get_next_line(int const fd, char **line)
 {
-		char		buf[BUFF_SIZE + 1];
-		static char	*str[10];
-		char		*ptr;
+	char		buf[BUFF_SIZE + 1];
+	static char	*str[10];
+	char		*ptr;
 
-		if ((read(fd, buf, 0) < 0) || BUFF_SIZE < 1 || !line || (fd < 0))
-				return (-1);
-		if (!str[fd])
-		{
-				str[fd] = ft_strnew(0);
-				if (str[fd] == NULL)
-						return (-1);
-		}
-		str[fd] = ft_really_get(fd, line, str);
-		ptr = str[fd];
-		if ((str[fd] = ft_get_remainder(ptr)) == NULL)
-		{
-				free(ptr);
-				return (0);
-		}
-		return (1);
+	if ((read(fd, buf, 0) < 0) || BUFF_SIZE < 1 || !line || (fd < 0))
+		return (-1);
+	if (!str[fd])
+	{
+		str[fd] = ft_strnew(0);
+		if (str[fd] == NULL)
+			return (-1);
+	}
+	str[fd] = ft_really_get(fd, line, str);
+	ptr = str[fd];
+	if ((str[fd] = ft_get_remainder(ptr)) == NULL)
+	{
+		free(ptr);
+		return (0);
+	}
+	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,11 @@
 /*   By: areid <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 09:37:13 by areid             #+#    #+#             */
-/*   Updated: 2017/11/28 09:37:15 by areid            ###   ########.fr       */
+/*   Updated: 2018/02/03 10:22:54 by areid            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 1
 
@@ -20,4 +20,4 @@
 
 int get_next_line(const int fd, char **line);
 
-#endif // GET_NEXT_LINE_H
+#endif
